9653fa3a4939da307b910caf4986643865656006
jameswquinn <jameswquinn@users.noreply.github.com>
2017-10-18 20:34:11 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,23 +1,7 @@
 const css = require('./Furtive/all.scss');
+var view = require('./template.marko'); // Import `./view.marko`
+var resultPromise = view.render({});
 
-function getImage(image) {return require('./uploads/' + image)};
-
-// loaded via srcset loader as specified in the webpack config
-/*
-['logo.jpeg', 'villianess.jpeg', 'villianess2.jpeg', 'villianess3.jpeg', 'villianess4.jpeg', 'villianess5.jpeg', 'villianess6.jpeg', 'villianess7.jpeg', 'villianess8.jpeg', 'villianess9.jpeg', 'villianess10.jpeg'].map(img => getImage(img)).forEach((src) => {
-  const image = new Image();
-  image.className = 'lazyload'
-  image.srcset = src.srcSet;
-  image.src = src.sources['800w'];
-  image.sizes = '(max-width: 1400px) 100vw, 1400px';
-  image.style = 'width: 100%';
-  const container = document.getElementById ("container");
-  container.appendChild(image);
+resultPromise.then((result) => {
+    result.appendTo(document.body);
 });
-*/
-
-
-var view = require('./intro.marko'); // Import `./view.marko`
-var result = view.renderSync({});
-
-result.appendTo(document.body);
--- webpack.config.js
+++ webpack.config.js
@@ -68,140 +68,124 @@ module.exports = {
         open: true
     },
     plugins: [
-      /*
+        /*
+          new HtmlWebpackPlugin({
+              title: 'Project Demo',
+              // minify: { collapseWhitespace: true },
+              inject: 'body',
+              hash: true,
+              template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+          }),
+          */
+        new HtmlWebpackPlugin({
+            // Required
+            inject: 'body',
+            template: './src/index.ejs',
+            // template: 'node_modules/html-webpack-template/index.ejs',
+            minify: {
+                collapseWhitespace: true
+            },
+
+            // Optional
+            appMountId: 'myApp',
+            //appMountIds: ['about', 'contact'], //e.g. multiple templates
+            meta: [{
+                name: 'identifier-url',
+                content: 'http://www.webestools.com/'
+            }, {
+                name: 'title',
+                content: 'Webestools'
+            }, {
+                name: 'description',
+                content: 'A better default template for html-webpack-plugin.'
+            }, {
+                name: 'abstract',
+                content: 'Tools for webmasters'
+            }, {
+                name: 'keywords',
+                content: 'tools, webmasters, meta, generator'
+            }, {
+                name: 'author',
+                content: 'A N Other'
+            }, {
+                name: 'revisit-after',
+                content: '15'
+            }, {
+                name: 'language',
+                content: 'EN'
+            }, {
+                name: 'copyright',
+                content: '© 2017 Webestool'
+            }, {
+                name: 'robots',
+                content: 'Index, follow'
+            }, ],
+            og: [{
+                property: 'title:og',
+                content: 'Webestools'
+            }, {
+                property: 'og:site_name',
+                content: 'David Walsh Blog'
+            }, {
+                property: 'image:og',
+                content: './src/logo.png'
+            }, {
+                property: 'description:og',
+                content: 'A better default template for html-webpack-plugin.'
+            }, ],
+            mobile: true,
+            lang: 'en',
+            title: 'Project Demo',
+            // minify: {
+            //     collapseWhitespace: true
+            // },
+            hash: true,
+            // And any other config options from html-webpack-plugin:
+            // https://github.com/ampedandwired/html-webpack-plugin#configuration
+        }),
+        /*
         new HtmlWebpackPlugin({
             title: 'Project Demo',
-            // minify: { collapseWhitespace: true },
-            inject: 'body',
+            // minify: {
+            //     collapseWhitespace: true
+            // },
             hash: true,
             template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
         }),
         */
-        new HtmlWebpackPlugin({
-     // Required
-     inject: 'body',
-     template: './src/index.ejs',
-     // template: 'node_modules/html-webpack-template/index.ejs',
-      minify: {
-          collapseWhitespace: true
-      },
-
-     // Optional
-     appMountId: 'myApp',
-     //appMountIds: ['about', 'contact'], //e.g. multiple templates
-     meta: [
-       {
-         name: 'identifier-url',
-         content: 'http://www.webestools.com/'
-       },
-       {
-         name: 'title',
-         content: 'Webestools'
-       },
-       {
-         name: 'description',
-         content: 'A better default template for html-webpack-plugin.'
-       },
-       {
-         name: 'abstract',
-         content: 'Tools for webmasters'
-       },
-       {
-         name: 'keywords',
-         content: 'tools, webmasters, meta, generator'
-       },
-       {
-         name: 'author',
-         content: 'A N Other'
-       },
-       {
-         name: 'revisit-after',
-         content: '15'
-       },
-       {
-         name: 'language',
-         content: 'EN'
-       },
-       {
-         name: 'copyright',
-         content: '© 2017 Webestool'
-       },
-       {
-         name: 'robots',
-         content: 'Index, follow'
-       },
-     ],
-     og: [
-       {
-         property: 'title:og',
-         content: 'Webestools'
-       },
-       {
-         property: 'og:site_name',
-         content: 'David Walsh Blog'
-       },
-       {
-         property: 'image:og',
-         content: './src/logo.png'
-       },
-       {
-         property: 'description:og',
-         content: 'A better default template for html-webpack-plugin.'
-       },
-     ],
-     mobile: true,
-     lang: 'en',
-     title: 'Project Demo',
-     // minify: {
-     //     collapseWhitespace: true
-     // },
-     hash: true,
-     // And any other config options from html-webpack-plugin:
-     // https://github.com/ampedandwired/html-webpack-plugin#configuration
-   }),
-       /*
-       new HtmlWebpackPlugin({
-           title: 'Project Demo',
-           // minify: {
-           //     collapseWhitespace: true
-           // },
-           hash: true,
-           template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
-       }),
-       */
         new FaviconsWebpackPlugin({
-    // Your source logo
-    logo: './src/logo.png',
-    // The prefix for all image files (might be a folder or a name)
-    prefix: 'icons-[hash]/',
-    // Emit all stats of the generated icons
-    emitStats: false,
-    // The name of the json containing all favicon information
-    statsFilename: 'iconstats-[hash].json',
-    // Generate a cache file with control hashes and
-    // don't rebuild the favicons until those hashes change
-    persistentCache: true,
-    // Inject the html into the html-webpack-plugin
-    inject: true,
-    // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
-    background: '#fff',
-    // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-    title: 'Webpack App',
+            // Your source logo
+            logo: './src/logo.png',
+            // The prefix for all image files (might be a folder or a name)
+            prefix: 'icons-[hash]/',
+            // Emit all stats of the generated icons
+            emitStats: false,
+            // The name of the json containing all favicon information
+            statsFilename: 'iconstats-[hash].json',
+            // Generate a cache file with control hashes and
+            // don't rebuild the favicons until those hashes change
+            persistentCache: true,
+            // Inject the html into the html-webpack-plugin
+            inject: true,
+            // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+            background: '#fff',
+            // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+            title: 'Webpack App',
 
-    // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
-    icons: {
-      android: true,
-      appleIcon: true,
-      appleStartup: false,
-      coast: false,
-      favicons: true,
-      firefox: false,
-      opengraph: false,
-      twitter: false,
-      yandex: false,
-      windows: false
-    }
-  }),
+            // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: false,
+                coast: false,
+                favicons: true,
+                firefox: false,
+                opengraph: false,
+                twitter: false,
+                yandex: false,
+                windows: false
+            }
+        }),
         new ExtractTextPlugin({
             filename: 'app.css',
             disable: false,
